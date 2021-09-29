6530d6968206d5e26602b00948ad0f97fd8040ad
Lanse <simon_stalnabb@hotmail.com>
2021-09-29 19:44:42 +0200

    Final push before bed

--- server/server.js
+++ server/server.js
@@ -1,17 +1,43 @@
-const express = require('express') // Gets the ExpressJS Module
-const app = express() // Setup the ExpressJS App
-const port = 3000 // Stores the Port Constant
+const express = require('express'); // Gets the ExpressJS Module
+const app = express(); // Setup the ExpressJS App
+const port = 3000; // Stores the Port Constant
 
-const path = require('path') // Gets the NodeJS Path Module
-const root = path.join(__dirname, '../') // Walks one step out of the package to get to the project root
-const website = path.join(root, 'website') // Walks one step into the 'website' package
+const path = require('path'); // Gets the NodeJS Path Module
+const root = path.join(__dirname, '../'); // Walks one step out of the package to get to the project root
+const website = path.join(root, 'website'); // Walks one step into the 'website' package
 
-// If the server gets a GET request for http://localhost:3000 then it returns the index.html file found in root/website/
+// Index Page
 app.get('/', function(req, res) {
   res.sendFile(path.join(website, 'index.html'));
 });
 
+// 404 Page
+app.get('/404', function(req, res) {
+  res.sendFile(path.join(website, '404.html'));
+});
+
 // Starts to listen to port 3000 for requests and such
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// Handle 404 Errors
+// DO NOTE: This has to be placed **>> LAST <<** in the call chain of both use and get implementations.
+app.use(function(req, res, next) {
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.redirect('/404');
+    return;
+  }
+
+  if (req.accepts('json')) {
+    res.json( { error: 'Not Found'} );
+    return;
+  }
+
+  if (req.accepts('txt')) {
+    res.type('txt').send('Not Found')
+    return;
+  }
+});
